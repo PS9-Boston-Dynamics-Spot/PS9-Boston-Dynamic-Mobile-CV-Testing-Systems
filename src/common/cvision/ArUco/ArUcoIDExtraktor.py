14a807c1a3c765bae69f111777a01711cd598e77
Kevin Pietzsch <s83799@htw-dresden.de>
2025-11-15 18:41:54 +0000

    add simple aruco extractor

---
+++ src/common/cvision/ArUco/ArUcoIDExtraktor.py
@@ -0,0 +1,35 @@
+import cv2
+import cv2.aruco as aruco
+from pathlib import Path
+from typing import List
+import numpy as np
+
+class ArUcoIDExtraktor:
+
+    def __init__(self) -> None:
+
+        self.dict = aruco.DICT_6X6_250 # TODO: outsource into a .config file
+        self.aruco_dict = aruco.getPredefinedDictionary(self.dict)
+        self.parameters = aruco.DetectorParameters()
+        self.detector = aruco.ArucoDetector(self.aruco_dict, self.parameters)
+
+        return
+    
+    def get_id(self, file_path: str) -> List[int]: # TODO: outsource into SQL + Minio
+
+        if not Path(file_path).exists():
+            raise FileNotFoundError
+
+        image = cv2.imread(file_path)
+
+        _, ids, _ = self.detector.detectMarkers(image)
+
+        return ids.flatten().tolist()
+
+
+
+if __name__ == "__main__":
+
+    extraktor = ArUcoIDExtraktor()
+    print(extraktor.get_id("./ArUco(1).jpeg"))
+    print(type(extraktor.get_id("./ArUco(1).jpeg")))
